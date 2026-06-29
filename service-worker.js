628bd6b8101de90acbfcd9fc52fdc2569db1017f
rifkilabbaik <rifkilabbaik@gmail.com>
2026-06-29 12:05:36 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,86 @@
+const CACHE_NAME = "penilaian-7lp-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+
+    self.skipWaiting();
+
+});
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys.map(key => {
+
+                    if (key !== CACHE_NAME) {
+
+                        return caches.delete(key);
+
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+        .then(response => {
+
+            return response || fetch(event.request)
+
+            .then(networkResponse => {
+
+                if (
+                    event.request.method === "GET" &&
+                    networkResponse.status === 200
+                ) {
+
+                    const clone = networkResponse.clone();
+
+                    caches.open(CACHE_NAME)
+                    .then(cache => cache.put(event.request, clone));
+
+                }
+
+                return networkResponse;
+
+            });
+
+        })
+
+        .catch(() => {
+
+            return caches.match("./index.html");
+
+        })
+
+    );
+
+});
